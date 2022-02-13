b2da5daba5a97e9eecbdef3dedbe0cb5d60b7eb4
쿠도커뮤니케이션 천명원 <mwcheon@cudo.co.kr>
2022-02-14 08:33:41 +0900

    간단한 화살표 함수

---
+++ es6/arrow-function.js
@@ -0,0 +1,19 @@
+//es5 - 함수 선언문
+function sum(a,b){
+    return a+b;
+}
+
+//es5 - 함수 표현식
+let sum = function(a,b){
+    return a+b;
+}
+
+//es6 - 함수 표현식
+let sum2 = (a,b)=>{
+    return a+b;
+}
+// let sum2 = (a:number,b:number):number=>{
+//     return a+b;
+// }
+// let sum3 = (a:number,b:number):number => a+b;
+
